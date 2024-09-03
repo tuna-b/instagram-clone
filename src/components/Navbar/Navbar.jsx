325447dac32ce92aaec11f51334629466216cb14
tuna-b <71737353+tuna-b@users.noreply.github.com>
2024-09-03 16:35:50 -0400

    Added Navbar UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,35 @@
 
-import { Box, Flex } from "@chakra-ui/react"
+import { Avatar, Box, Flex, Link, Tooltip } from "@chakra-ui/react"
 import { Link as RouterLink} from 'react-router-dom';
+import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from "../../assets/constants";
+import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
 const Navbar = () => {
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo />,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: "Create"
+    },
+    {
+      icon: <Avatar size={"sm"} name="Tuna Bolukbasi" src="/profilepic.png" />,
+      text: "Profile",
+      link: "/sampleprofilelink"
+    }
+  ]
   return (
     <Box 
     height={"100vh"}
@@ -15,8 +42,68 @@ const Navbar = () => {
     px={{base: 2, md:4}}
     >
         <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
-            <RouterLink to={"/"} pl={2} display={{base: "none", md:"block"}}>
-            </RouterLink>
+            <Link to={"/"} as={RouterLink} pl={2} display={{base: "none", md:"block"}}>
+            <InstagramLogo />
+            </Link>
+            <Link to={"/"} as={RouterLink} p={2} display={{base: "block", md:"none"}}
+            borderRadius={6} _hover={{bg:"whiteAlpha.200"}} w={{base:10}}>
+            <InstagramMobileLogo />
+            </Link>
+            <Flex direction={"column"} gap={5} cursor={"pointer"}>
+              {sidebarItems.map((items, index) => (
+                <Tooltip 
+                key={index}
+                hasArrow
+                label={items.text}
+                placement="right"
+                ml={1}
+                openDelay={500}
+                display={{base: 'block', md: 'none'}}
+                >
+                <Link
+                display={"flex"}
+                to={items.link || null}
+                as={RouterLink}
+                alignItems={"center"}
+                gap={4}
+                _hover={{bg: 'whiteAlpha.400'}}
+                borderRadius={6}
+                p={2}
+                w={{base: '10', md: 'full'}}
+                justifyContent={{base: 'center', md: 'flex-start'}}
+                >
+                  {items.icon}
+                  <Box display={{base: 'none', md: 'block'}}>{items.text}</Box>
+                </Link>
+                </Tooltip>
+              ))}
+
+            </Flex>
+            <Tooltip 
+                hasArrow
+                label={'Logout'}
+                placement="right"
+                ml={1}
+                openDelay={500}
+                display={{base: 'block', md: 'none'}}
+                >
+                <Link
+                display={"flex"}
+                to={'/auth'}
+                as={RouterLink}
+                alignItems={"center"}
+                gap={4}
+                _hover={{bg: 'whiteAlpha.400'}}
+                borderRadius={6}
+                p={2}
+                w={{base: '10', md: 'full'}}
+                mt={'auto'}
+                justifyContent={{base: 'center', md: 'flex-start'}}
+                >
+                  <BiLogOut size={25} />
+                  <Box display={{base: 'none', md: 'block'}}>Logout</Box>
+                </Link>
+                </Tooltip>
         </Flex>
     </Box>
   )
